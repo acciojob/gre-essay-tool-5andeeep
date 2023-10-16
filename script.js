9129d3271a227bb1589abfa24b4914347f611245
Sandeep <newsandeep03@gmail.com>
2023-10-16 19:46:26 +0530

    ans added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+
+function countWords(){
+    let splitText = document.querySelector('#evaluatedText').value;
+    let words = splitText.split(" ");
+    let count = 0;
+    for(let i=0; i<words.length; i++){
+        if(words[i] !== " "){
+            count++;
+        }
+    }
+    console.log(count);
+    document.querySelector("#wordCount").innerHTML = count;
+}
